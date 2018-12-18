70b6964b23d5b6d59a0a9bd9ecd80366d1805b10
Lay4U <kmr345@naver.com>
2018-12-18 22:36:21 +0900

    ranking implement complete

--- WebContent/WEB-INF/classes/user/UserDAO.java
+++ WebContent/WEB-INF/classes/user/UserDAO.java
@@ -1,18 +1,41 @@
 package user;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import java.util.ArrayList;
 
 public class UserDAO {
 
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
+
+	public ArrayList<User> getRank() {
+		String SQL = "select * from user order by userPoint desc";
+		ArrayList<User> list = new ArrayList<User>();
+		try {
+
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				User us = new User();
+				us.setUserID(rs.getString(1));
+				us.setUserPassword(rs.getString(2));
+				us.setUserName(rs.getString(3));
+				us.setUserGender(rs.getString(4));
+				us.setUserEmail(rs.getString(5));
+				us.setUserPoint(rs.getInt(6));
+				list.add(us);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
 	public UserDAO() {
 		try {
 			String dbURL = "jdbc:mysql://localhost:3306/bbs?serverTimezone=UTC&autoReconnect=true&useSSL=false";
@@ -20,32 +43,31 @@ public class UserDAO {
 			String dbPassword = "kgu123";
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public int login(String userID, String userPassword) {
 		String SQL = "SELECT userPassword from USER where userID = ?";
 		try {
 			pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1,  userID);
+			pstmt.setString(1, userID);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				if(rs.getString(1).equals(userPassword)) {
-					return 1; //로그인 성공
-				}
-				else
-					return 0; //비밀번호 불일치
+				if (rs.getString(1).equals(userPassword)) {
+					return 1; // 로그인 성공
+				} else
+					return 0; // 비밀번호 불일치
 			}
-			return -1; //아이디가 없음
+			return -1; // 아이디가 없음
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -2; //데이터베이스 오류
+		return -2; // 데이터베이스 오류
 	}
-	
+
 	public int join(User user) {
 		String SQL = "insert into user values ( ?, ?, ?, ?, ?)";
 		try {
@@ -61,5 +83,7 @@ public class UserDAO {
 		}
 		return -1; // 데이터베이스 오류
 	}
+
 	
+
 }
